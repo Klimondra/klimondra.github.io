a3d974dfb6aac1bc44f5ac060727b22443993b1f
Klimondra <78172612+Klimondra@users.noreply.github.com>
2023-08-10 19:10:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nam-menu");
+    
+    hamburger.addEventListener("click", function() {
+        navMenu.classList.toggle("active");
+    });
+});
 
-hamburegr.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-ling").forEach(n = n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
