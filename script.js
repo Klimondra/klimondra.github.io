0aef8de4a2af472f95e77b3c25a1290b201c1e31
Klimondra <78172612+Klimondra@users.noreply.github.com>
2023-08-10 19:18:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
     hamburger.addEventListener("click", function() {
         navMenu.classList.toggle("active");
     });
-});
 
+    document.addEventListener("click", function(event) {
+        if (!navMenu.contains(event.target) && !hamburger.contains(event.target)) {
+            navMenu.classList.remove("active");
+        }
+    });
+});
